367cef85e15e2e100fe92061471a18c2ee2dc538
La-mouv <147877974+La-mouv@users.noreply.github.com>
2023-10-16 21:44:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ document.getElementById('addTask').addEventListener('click', function() {
         document.getElementById('newTask').value = '';  // Réinitialiser le champ de saisie
     }
 });
+
+function addTask() {
+    const taskValue = document.getElementById('newTask').value;
+    if (taskValue) {
+        const li = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.addEventListener('change', function() {
+            if (this.checked) {
+                li.style.textDecoration = 'line-through';
+                li.style.color = '#aaa';
+            } else {
+                li.style.textDecoration = 'none';
+                li.style.color = '#000';
+            }
+        });
+
+        li.appendChild(checkbox);
+        li.appendChild(document.createTextNode(' ' + taskValue));
+        document.getElementById('tasksList').appendChild(li);
+        document.getElementById('newTask').value = ''; // Réinitialiser le champ de saisie
+    }
+}
+
+document.getElementById('addTask').addEventListener('click', addTask);
+document.getElementById('newTask').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
